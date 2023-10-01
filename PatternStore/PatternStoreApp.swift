939adac98769b66d27436933293f7f2abe96464a
Anton <antony.adamson@gmail.com>
2023-10-01 11:34:18 +0600

    Initial Commit

---
+++ PatternStore/PatternStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatternStoreApp.swift
+//  PatternStore
+//
+//  Created by Антон Адамсон on 01.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PatternStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
